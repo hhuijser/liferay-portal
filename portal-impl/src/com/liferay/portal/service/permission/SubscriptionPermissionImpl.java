8f0baea9af4e79ee3ab57edc808f04aea68d4353
Matthew Kong <matthew.kong@liferay.com>
2011-10-25 14:14:11 -0700

    LPS-22019

--- portal-impl/src/com/liferay/portal/service/permission/SubscriptionPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/service/permission/SubscriptionPermissionImpl.java
@@ -16,9 +16,11 @@ package com.liferay.portal.service.permission;
 
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.Group;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portlet.blogs.model.BlogsEntry;
 import com.liferay.portlet.blogs.service.permission.BlogsPermission;
 import com.liferay.portlet.journal.model.JournalArticle;
@@ -27,6 +29,7 @@ import com.liferay.portlet.messageboards.model.MBCategory;
 import com.liferay.portlet.messageboards.model.MBThread;
 import com.liferay.portlet.messageboards.service.permission.MBCategoryPermission;
 import com.liferay.portlet.messageboards.service.permission.MBMessagePermission;
+import com.liferay.portlet.messageboards.service.permission.MBPermission;
 import com.liferay.portlet.wiki.model.WikiNode;
 import com.liferay.portlet.wiki.model.WikiPage;
 import com.liferay.portlet.wiki.service.permission.WikiNodePermission;
@@ -63,8 +66,16 @@ public class SubscriptionPermissionImpl implements SubscriptionPermission {
 				permissionChecker, classPK, ActionKeys.SUBSCRIBE);
 		}
 		else if (className.equals(MBCategory.class.getName())) {
-			return MBCategoryPermission.contains(
-				permissionChecker, classPK, ActionKeys.SUBSCRIBE);
+			Group group = GroupLocalServiceUtil.fetchGroup(classPK);
+
+			if (group == null) {
+				return MBCategoryPermission.contains(
+					permissionChecker, classPK, ActionKeys.SUBSCRIBE);
+			}
+			else {
+				return MBPermission.contains(
+					permissionChecker, classPK, ActionKeys.SUBSCRIBE);
+			}
 		}
 		else if (className.equals(MBThread.class.getName())) {
 			return MBMessagePermission.contains(

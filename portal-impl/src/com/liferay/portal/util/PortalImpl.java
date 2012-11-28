b3a72fc9736453e783d4ba1c580cb3d4f0f2c214
Douglas Wong <douglas.wong@liferay.com>
2012-11-27 17:29:55 -0800

    LPS-25621 source formatting

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -15,6 +15,7 @@
 package com.liferay.portal.util;
 
 import com.liferay.portal.NoSuchCompanyException;
+import com.liferay.portal.NoSuchGroupException;
 import com.liferay.portal.NoSuchImageException;
 import com.liferay.portal.NoSuchLayoutException;
 import com.liferay.portal.NoSuchUserException;
@@ -5752,10 +5753,14 @@ public class PortalImpl implements Portal {
 			primaryKey = portletPrimaryKey;
 		}
 		else {
-			Group group = GroupLocalServiceUtil.getGroup(groupId);
+			try {
+				Group group = GroupLocalServiceUtil.getGroup(groupId);
 
-			if (group.isStagingGroup()) {
-				groupId = group.getLiveGroupId();
+				if (group.isStagingGroup()) {
+					groupId = group.getLiveGroupId();
+				}
+			}
+			catch (NoSuchGroupException nsge) {
 			}
 
 			name = ResourceActionsUtil.getPortletBaseResource(rootPortletId);

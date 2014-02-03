3662001a77ff94c2d56ac4ed02cca0dc6b0efc58
Jonathan McCann <jonathan.mccann@liferay.com>
2014-02-03 15:09:05 -0800

    LPS-43991 Source Formatting

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -4625,13 +4625,12 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		user.setPasswordUnencrypted(password1);
 		user.setPasswordEncrypted(true);
 		user.setPasswordReset(passwordReset);
-
-		if (Validator.isNull(user.getPasswordModifiedDate())) {
-			user.setPasswordModifiedDate(new Date());
-		}
-
 		user.setDigest(StringPool.BLANK);
 		user.setGraceLoginCount(0);
+
+		if (user.getPasswordModifiedDate() == null) {
+			user.setPasswordModifiedDate(new Date());
+		}
 
 		if (!silentUpdate) {
 			user.setPasswordModifiedDate(new Date());

1d695a476cfd9605927adf6b726aba77b13af438
willnewbury <william.newbury@liferay.com>
2014-10-17 09:30:48 -0700

    LPS-49294 Exempt fields annoatated with @BeanReference, @SuppressWarnings, or @Mock

--- portal-impl/src/com/liferay/portal/tools/sourceformatter/JavaSourceProcessor.java
+++ portal-impl/src/com/liferay/portal/tools/sourceformatter/JavaSourceProcessor.java
@@ -26,6 +26,7 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import com.thoughtworks.qdox.JavaDocBuilder;
+import com.thoughtworks.qdox.model.Annotation;
 import com.thoughtworks.qdox.model.ClassLibrary;
 import com.thoughtworks.qdox.model.JavaField;
 import com.thoughtworks.qdox.model.JavaMethod;
@@ -213,6 +214,23 @@ public class JavaSourceProcessor extends BaseSourceProcessor {
 		JavaField javaField, String content) {
 
 		Type javaClassType = javaClass.asType();
+		Annotation[] annotations = javaField.getAnnotations();
+
+		List<String> annotationExclusions = new ArrayList<String>();
+
+		annotationExclusions.add(
+			"com.liferay.portal.kernel.bean.BeanReference");
+		annotationExclusions.add("org.mockito.Mock");
+		annotationExclusions.add("java.lang.SuppressWarnings");
+
+		for (Annotation annotation: annotations) {
+			Type annotationType = annotation.getType();
+			String annotationTypeString = annotationType.toString();
+
+			if (annotationExclusions.contains(annotationTypeString)) {
+				return content;
+			}
+		}
 
 		if ((javaClass.isEnum() && javaClassType.equals(javaField.getType())) ||
 			javaField.isFinal()) {

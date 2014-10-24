6d033a6b135b03c03e0750deaf59a8876c5ca53f
hai.yu <8219754@liferay.com>
2014-10-24 23:33:54 +0800

    LPS-51059 keep the original logic "Rebuild tree after category with children gets deleted"

--- portal-impl/src/com/liferay/portlet/asset/service/impl/AssetCategoryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/impl/AssetCategoryLocalServiceImpl.java
@@ -252,11 +252,15 @@ public class AssetCategoryLocalServiceImpl
 			assetCategoryPersistence.findByParentCategoryId(
 				category.getCategoryId());
 
+		if (categories.size() > 0) {
+			_isHaveChildCategory = true;
+		}
+
 		for (AssetCategory curCategory : categories) {
 			deleteCategory(curCategory, true, false);
 		}
 
-		if (rebuildTree) {
+		if (rebuildTree && _isHaveChildCategory) {
 			final long groupId = category.getGroupId();
 
 			TransactionCommitCallbackRegistryUtil.registerCallback(
@@ -265,6 +269,8 @@ public class AssetCategoryLocalServiceImpl
 					@Override
 					public Void call() throws Exception {
 						assetCategoryLocalService.rebuildTree(groupId, true);
+
+						_isHaveChildCategory = false;
 
 						return null;
 					}
@@ -831,4 +837,5 @@ public class AssetCategoryLocalServiceImpl
 		}
 	}
 
+	private boolean _isHaveChildCategory;
 }

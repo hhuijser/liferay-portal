1c487197f3f9f21388e11f879577a745a579ec6f
hai.yu <8219754@liferay.com>
2015-03-24 16:33:22 +0800

    LPS-54454 Adding groupIds param method to invoke new G_C_C method

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMStructureLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/impl/DDMStructureLocalServiceImpl.java
@@ -746,6 +746,76 @@ public class DDMStructureLocalServiceImpl
 	}
 
 	/**
+	 * Returns all the structures matching the groups and class name ID.
+	 *
+	 * @param  groupIds the primary keys of the groups
+	 * @param  companyId the primary key of the structure's company
+	 * @param  classNameId the primary key of the class name for the structure's
+	 *         related model
+	 * @return the structures matching the class name ID
+	 */
+	@Override
+	public List<DDMStructure> getClassStructures(
+		long[] groupIds, long companyId, long classNameId) {
+
+		return ddmStructurePersistence.filterFindByG_C_C(
+			groupIds, companyId, classNameId);
+	}
+
+	/**
+	 * Returns a range of all the structures matching the groups and class name
+	 * ID.
+	 *
+	 * <p>
+	 * Useful when paginating results. Returns a maximum of <code>end -
+	 * start</code> instances. <code>start</code> and <code>end</code> are not
+	 * primary keys, they are indexes in the result set. Thus, <code>0</code>
+	 * refers to the first result in the set. Setting both <code>start</code>
+	 * and <code>end</code> to {@link
+	 * com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full
+	 * result set.
+	 * </p>
+	 *
+	 * @param  groupIds the primary keys of the groups
+	 * @param  companyId the primary key of the structure's company
+	 * @param  classNameId the primary key of the class name for the structure's
+	 *         related model
+	 * @param  start the lower bound of the range of structures to return
+	 * @param  end the upper bound of the range of structures to return (not
+	 *         inclusive)
+	 * @return the range of matching structures
+	 */
+	@Override
+	public List<DDMStructure> getClassStructures(
+		long[] groupIds, long companyId, long classNameId, int start, int end) {
+
+		return ddmStructurePersistence.filterFindByG_C_C(
+			groupIds, companyId, classNameId, start, end);
+	}
+
+	/**
+	 * Returns all the structures matching the groups and class name ID ordered
+	 * by the comparator.
+	 *
+	 * @param  groupIds the primary keys of the groups
+	 * @param  companyId the primary key of the structure's company
+	 * @param  classNameId the primary key of the class name for the structure's
+	 *         related model
+	 * @param  orderByComparator the comparator to order the structures
+	 *         (optionally <code>null</code>)
+	 * @return the matching structures ordered by the comparator
+	 */
+	@Override
+	public List<DDMStructure> getClassStructures(
+		long[] groupIds, long companyId, long classNameId,
+		OrderByComparator<DDMStructure> orderByComparator) {
+
+		return ddmStructurePersistence.filterFindByG_C_C(
+			groupIds, companyId, classNameId, QueryUtil.ALL_POS,
+			QueryUtil.ALL_POS, orderByComparator);
+	}
+
+	/**
 	 * Returns all the structures for the document library file entry type.
 	 *
 	 * @param  dlFileEntryTypeId the primary key of the document library file

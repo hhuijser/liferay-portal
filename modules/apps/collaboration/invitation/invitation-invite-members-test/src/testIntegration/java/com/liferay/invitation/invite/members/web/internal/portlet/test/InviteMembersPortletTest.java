87b0fdc150c0bf2026eec8f390144d72b911ee71
Hugo Huijser <hugo.huijser@liferay.com>
2017-11-17 08:27:11 +0800

    LPS-1

--- modules/apps/collaboration/invitation/invitation-invite-members-test/src/testIntegration/java/com/liferay/invitation/invite/members/web/internal/portlet/test/InviteMembersPortletTest.java
+++ modules/apps/collaboration/invitation/invitation-invite-members-test/src/testIntegration/java/com/liferay/invitation/invite/members/web/internal/portlet/test/InviteMembersPortletTest.java
@@ -15,9 +15,10 @@
 package com.liferay.invitation.invite.members.web.internal.portlet.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.portal.dao.orm.custom.sql.CustomSQLUtil;
 import com.liferay.portal.kernel.dao.orm.CustomSQLParam;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
@@ -27,9 +28,13 @@ import com.liferay.portal.kernel.test.rule.Sync;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.kernel.test.util.UserGroupTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.comparator.UserFirstNameComparator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.io.IOException;
 
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -99,19 +104,58 @@ public class InviteMembersPortletTest {
 
 		LinkedHashMap usersParams = new LinkedHashMap();
 
-		usersParams.put(
-			"usersInvited",
-			new CustomSQLParam(
-				CustomSQLUtil.get(
-					getClass(),
-					"com.liferay.portal.service.persistence.UserFinder." +
-						"filterByUsersGroupsGroupId"),
-				groupId));
+		usersParams.put("usersInvited", new CustomSQLParam(_getSQL(), groupId));
 
 		return UserLocalServiceUtil.search(
 			companyId, null, WorkflowConstants.STATUS_ANY, usersParams,
 			QueryUtil.ALL_POS, QueryUtil.ALL_POS,
 			new UserFirstNameComparator(true));
+	}
+
+	private String _getSQL() {
+		StringBundler sb = new StringBundler();
+
+		sb.append("WHERE\n");
+		sb.append("User_.userId NOT IN\n");
+		sb.append("(\n");
+		sb.append("SELECT\n");
+		sb.append("DISTINCT Users_Groups.userId\n");
+		sb.append("FROM\n");
+		sb.append("Users_Groups\n");
+		sb.append("WHERE\n");
+		sb.append("Users_Groups.groupId = ?\n");
+		sb.append(")\n");
+
+		return _transform(sb.toString());
+	}
+
+	private String _transform(String sql) {
+		StringBundler sb = new StringBundler();
+
+		try (UnsyncBufferedReader unsyncBufferedReader =
+				new UnsyncBufferedReader(new UnsyncStringReader(sql))) {
+
+			String line = null;
+
+			while ((line = unsyncBufferedReader.readLine()) != null) {
+				line = line.trim();
+
+				if (line.startsWith(StringPool.CLOSE_PARENTHESIS)) {
+					sb.setIndex(sb.index() - 1);
+				}
+
+				sb.append(line);
+
+				if (!line.endsWith(StringPool.OPEN_PARENTHESIS)) {
+					sb.append(StringPool.SPACE);
+				}
+			}
+		}
+		catch (IOException ioe) {
+			return sql;
+		}
+
+		return sb.toString();
 	}
 
 	@DeleteAfterTestRun

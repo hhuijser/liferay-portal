ce4be06ea136576357080227d84e2b9d859913d0
Alan Huang <alan.huang@liferay.com>
2018-01-16 09:08:19 +0800

    LPS-77112 Remove duplicate parameters from url

--- portal-impl/src/com/liferay/portal/util/HttpImpl.java
+++ portal-impl/src/com/liferay/portal/util/HttpImpl.java
@@ -2041,6 +2041,8 @@ public class HttpImpl implements Http {
 
 		String[] params = StringUtil.split(queryString, CharPool.AMPERSAND);
 
+		params = ArrayUtil.unique(params);
+
 		List<String> redirectParams = new ArrayList<>();
 
 		for (String param : params) {

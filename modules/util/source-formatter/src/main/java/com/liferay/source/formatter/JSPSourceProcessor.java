27f3dba59aed774a9eec7eb9e489a2ed4c3401f9
Peter Shin <peter.shin@liferay.com>
2018-03-15 16:49:10 -0700

    LPS-78878 Add flag (no logic changes)

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JSPSourceProcessor.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/JSPSourceProcessor.java
@@ -57,7 +57,7 @@ public class JSPSourceProcessor extends BaseSourceProcessor {
 
 		return JSPSourceUtil.addIncludedAndReferencedFileNames(
 			fileNames, new HashSet<String>(),
-			JSPSourceUtil.getContentsMap(allJSPFileNames));
+			JSPSourceUtil.getContentsMap(allJSPFileNames), false);
 	}
 
 	@Override

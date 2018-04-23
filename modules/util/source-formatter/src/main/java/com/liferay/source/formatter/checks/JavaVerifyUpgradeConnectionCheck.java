88ed6a907de927a8ad28dd69e4926624d8651777
Peter Shin <peter.shin@liferay.com>
2018-04-23 10:56:57 -0700

    LPS-79963 Check connections for every class that is a subclass of 'UpgradeProcess'

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaVerifyUpgradeConnectionCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/JavaVerifyUpgradeConnectionCheck.java
@@ -14,12 +14,43 @@
 
 package com.liferay.source.formatter.checks;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.tools.JavaImportsFormatter;
 import com.liferay.source.formatter.checks.util.JavaSourceUtil;
+import com.liferay.source.formatter.checks.util.SourceUtil;
+import com.liferay.source.formatter.util.FileUtil;
+
+import java.io.File;
+
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Hugo Huijser
  */
 public class JavaVerifyUpgradeConnectionCheck extends BaseFileCheck {
+
+	@Override
+	public void init() throws Exception {
+		_absolutePaths.addAll(_getUpgradeAbsolutePaths());
+	}
 
 	@Override
 	public boolean isPortalCheck() {
@@ -28,20 +59,20 @@ public class JavaVerifyUpgradeConnectionCheck extends BaseFileCheck {
 
 	@Override
 	protected String doProcess(
-		String fileName, String absolutePath, String content) {
+			String fileName, String absolutePath, String content)
+		throws Exception {
 
-		if (absolutePath.contains("/test/") ||
-			fileName.endsWith("DBUpgrader.java") ||
-			fileName.endsWith("Test.java") ||
-			fileName.endsWith("UpgradeTableListener.java") ||
-			content.contains("Callable<Void>")) {
-
+		if (absolutePath.contains("/test/")) {
 			return content;
 		}
 
 		String className = JavaSourceUtil.getClassName(fileName);
 
 		if (!className.contains("Upgrade") && !className.contains("Verify")) {
+			return content;
+		}
+
+		if (!_extendsPortalKernelUpgradeProcess(absolutePath, content)) {
 			return content;
 		}
 
@@ -63,5 +94,157 @@ public class JavaVerifyUpgradeConnectionCheck extends BaseFileCheck {
 
 		return content;
 	}
+
+	private boolean _extendsPortalKernelUpgradeProcess(
+			String absolutePath, String content)
+		throws Exception {
+
+		String upgradeAbsolutePath = absolutePath;
+		String upgradeContent = content;
+
+		while (Validator.isNotNull(upgradeAbsolutePath)) {
+			if (!absolutePath.equals(upgradeAbsolutePath)) {
+				upgradeContent = _getUpgradeContent(upgradeAbsolutePath);
+			}
+
+			upgradeAbsolutePath = _getExtendedClassAbsolutePath(
+				upgradeAbsolutePath, upgradeContent);
+
+			if (upgradeAbsolutePath.endsWith(
+					"com/liferay/portal/kernel/upgrade/UpgradeProcess.java")) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private String _getExtendedClassAbsolutePath(
+		String absolutePath, String content) {
+
+		Matcher matcher = _extendedClassPattern.matcher(content);
+
+		if (!matcher.find()) {
+			return StringPool.BLANK;
+		}
+
+		String extendedClassName = matcher.group(1);
+
+		if (!extendedClassName.contains(StringPool.PERIOD)) {
+			String[] importLines = StringUtil.splitLines(
+				JavaImportsFormatter.getImports(content));
+
+			for (String importLine : importLines) {
+				if (Validator.isNull(importLine)) {
+					continue;
+				}
+
+				String importName = importLine.substring(
+					7, importLine.length() - 1);
+
+				if (importName.endsWith(CharPool.PERIOD + extendedClassName)) {
+					extendedClassName = importName;
+
+					break;
+				}
+			}
+		}
+
+		String s = StringPool.BLANK;
+
+		if (!extendedClassName.contains(StringPool.PERIOD)) {
+			int x = absolutePath.lastIndexOf(CharPool.SLASH);
+
+			s = absolutePath.substring(0, x + 1) + extendedClassName;
+		}
+		else {
+			s = StringUtil.replace(
+				extendedClassName, CharPool.PERIOD, CharPool.SLASH);
+		}
+
+		for (String upgradeAbsolutePath : _absolutePaths) {
+			if (upgradeAbsolutePath.endsWith(s + ".java")) {
+				return upgradeAbsolutePath;
+			}
+		}
+
+		return StringPool.BLANK;
+	}
+
+	private List<String> _getUpgradeAbsolutePaths() throws Exception {
+		File portalDir = getPortalDir();
+
+		if (portalDir == null) {
+			return Collections.emptyList();
+		}
+
+		final List<String> upgradeAbsolutePaths = new ArrayList<>();
+
+		Files.walkFileTree(
+			portalDir.toPath(), EnumSet.noneOf(FileVisitOption.class), 20,
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult preVisitDirectory(
+					Path dirPath, BasicFileAttributes basicFileAttributes) {
+
+					String dirName = String.valueOf(dirPath.getFileName());
+
+					if (ArrayUtil.contains(_SKIP_DIR_NAMES, dirName)) {
+						return FileVisitResult.SKIP_SUBTREE;
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFile(
+					Path filePath, BasicFileAttributes basicFileAttributes) {
+
+					String absolutePath = SourceUtil.getAbsolutePath(filePath);
+					String fileName = String.valueOf(filePath.getFileName());
+
+					if (absolutePath.contains("/upgrade/") &&
+						fileName.endsWith(".java")) {
+
+						upgradeAbsolutePaths.add(absolutePath);
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+
+		return upgradeAbsolutePaths;
+	}
+
+	private String _getUpgradeContent(String absolutePath) throws Exception {
+		if (_contentsMap.containsKey(absolutePath)) {
+			return _contentsMap.get(absolutePath);
+		}
+
+		File file = new File(absolutePath);
+
+		if (!file.exists()) {
+			return StringPool.BLANK;
+		}
+
+		_contentsMap.put(absolutePath, FileUtil.read(file));
+
+		return _contentsMap.get(absolutePath);
+	}
+
+	private static final String[] _SKIP_DIR_NAMES = {
+		".git", ".gradle", ".idea", ".m2", ".settings", "bin", "build",
+		"classes", "dependencies", "node_modules", "sql", "test",
+		"test-classes", "test-coverage", "test-results", "tmp"
+	};
+
+	private static final Pattern _extendedClassPattern = Pattern.compile(
+		"\\sextends\\s+([\\w\\.]+)\\W");
+
+	private final List<String> _absolutePaths = new ArrayList<>();
+	private final Map<String, String> _contentsMap = new HashMap<>();
 
 }

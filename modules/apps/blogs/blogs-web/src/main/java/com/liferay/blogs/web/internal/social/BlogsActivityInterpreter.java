6b8c51bfe26dd21f9e12a7a3d5c8565df3b318e2
Carlos Sierra <carlos.sierra@liferay.com>
2018-06-20 09:41:22 +0200

    LPS-82668 Use `dynamic + greedy` to allow key overloading

--- modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/social/BlogsActivityInterpreter.java
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/social/BlogsActivityInterpreter.java
@@ -35,6 +35,8 @@ import java.text.Format;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Brian Wing Shun Chan
@@ -183,15 +185,6 @@ public class BlogsActivityInterpreter extends BaseSocialActivityInterpreter {
 		_blogsEntryLocalService = blogsEntryLocalService;
 	}
 
-	@Reference(
-		target = "(bundle.symbolic.name=com.liferay.blogs.web)", unbind = "-"
-	)
-	protected void setResourceBundleLoader(
-		ResourceBundleLoader resourceBundleLoader) {
-
-		_resourceBundleLoader = resourceBundleLoader;
-	}
-
 	private static final String[] _CLASS_NAMES = {BlogsEntry.class.getName()};
 
 	@Reference(target = "(model.class.name=com.liferay.blogs.model.BlogsEntry)")
@@ -199,6 +192,12 @@ public class BlogsActivityInterpreter extends BaseSocialActivityInterpreter {
 		_blogsEntryFolderModelResourcePermission;
 
 	private BlogsEntryLocalService _blogsEntryLocalService;
-	private ResourceBundleLoader _resourceBundleLoader;
+
+	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(bundle.symbolic.name=com.liferay.blogs.web)"
+	)
+	private volatile ResourceBundleLoader _resourceBundleLoader;
 
 }

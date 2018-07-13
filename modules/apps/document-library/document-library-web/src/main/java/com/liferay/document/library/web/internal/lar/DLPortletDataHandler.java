15e59a79a0abf292535a2eee440b67e1a7a933bb
arthurchan35 <arthurchan35@gmail.com>
2018-07-13 14:01:18 +0800

    LPS-83223 in PortletPreferncesFacotryImpl.getPortletSetup(), when the portlet's preferences is uniquePerLayout, it is expecting a plid > 0, but currently DLAdminPortlet and DLPortlet both use the same datahandler, and in the case of DLPortlet, it does not have a plid since it's not an instance on a page thus of having plid = 0

---
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/lar/DLPortletDataHandler.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.internal.lar;
+
+import com.liferay.document.library.constants.DLPortletKeys;
+import com.liferay.exportimport.kernel.lar.DataLevel;
+import com.liferay.exportimport.kernel.lar.PortletDataHandler;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author arthurchan35
+ */
+@Component(
+	immediate = true,
+	property = "javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY,
+	service = PortletDataHandler.class
+)
+public class DLPortletDataHandler extends DLAdminPortletDataHandler {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		setDataLevel(DataLevel.PORTLET_INSTANCE);
+		super.activate(bundleContext);
+	}
+
+}

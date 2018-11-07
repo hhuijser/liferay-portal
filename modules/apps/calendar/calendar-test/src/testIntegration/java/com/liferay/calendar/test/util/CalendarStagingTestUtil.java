cc77d3a230643803557dfd792c1eb702f8fd576e
arthurchan35 <arthurchan35@gmail.com>
2018-11-07 11:35:14 +0800

    LPS-86236 update test

--- modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/test/util/CalendarStagingTestUtil.java
+++ modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/test/util/CalendarStagingTestUtil.java
@@ -66,7 +66,7 @@ public class CalendarStagingTestUtil {
 
 		addStagingAttribute(
 			serviceContext,
-			StagingUtil.getStagedPortletId(CalendarPortletKeys.CALENDAR),
+			StagingUtil.getStagedPortletId(CalendarPortletKeys.CALENDAR_ADMIN),
 			enableCalendarStaging);
 		addStagingAttribute(
 			serviceContext, PortletDataHandlerKeys.PORTLET_CONFIGURATION_ALL,

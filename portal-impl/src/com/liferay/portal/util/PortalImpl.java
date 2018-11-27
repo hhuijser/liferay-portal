67f4236eccc6950a2f9e62e59d151d27316fd1a5
Alan Huang <alan.huang@liferay.com>
2018-11-27 17:48:35 +0800

    LPS-87843 Use getLayoutActualURL() to get the url if the url is not an external url

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -3310,6 +3310,9 @@ public class PortalImpl implements Portal {
 
 				return url;
 			}
+
+			return addPreservedParameters(
+				themeDisplay, layout, getLayoutActualURL(layout), doAsUser);
 		}
 
 		String layoutFriendlyURL = getLayoutFriendlyURL(layout, themeDisplay);

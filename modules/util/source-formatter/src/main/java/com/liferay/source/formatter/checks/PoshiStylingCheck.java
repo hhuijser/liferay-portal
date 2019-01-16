a47a91361e8ee38c493a7a099a28906833f181e9
Alan Huang <alan.huang@liferay.com>
2019-01-16 13:16:44 +0800

    LPS-84119 Add SortParameterNames

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PoshiStylingCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/PoshiStylingCheck.java
@@ -15,9 +15,18 @@
 package com.liferay.source.formatter.checks;
 
 import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.tools.ToolsUtil;
+import com.liferay.source.formatter.checks.util.SourceUtil;
 
 import java.io.IOException;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Alan Huang
@@ -30,6 +39,8 @@ public class PoshiStylingCheck extends BaseFileCheck {
 		throws IOException {
 
 		_checkLineBreak(fileName, content);
+
+		content = _sortParameterNames(content);
 
 		return content;
 	}
@@ -53,5 +64,66 @@ public class PoshiStylingCheck extends BaseFileCheck {
 			}
 		}
 	}
+
+	private String _sortParameterNames(String content) {
+		Matcher matcher = _variableNamesPattern.matcher(content);
+
+		while (matcher.find()) {
+			String indent = SourceUtil.getIndent(matcher.group());
+
+			String[] lines;
+			lines = StringUtil.splitLines(matcher.group(1));
+
+			Map<String, String> parametersMap = new TreeMap<>();
+
+			for (String line : lines) {
+				line = StringUtil.trim(line);
+
+				int equalPos = line.indexOf(" = ");
+
+				if (equalPos > 0) {
+					String parameterName;
+					String parameterValue;
+
+					parameterName = line.substring(0, equalPos);
+
+					if (line.endsWith(StringPool.COMMA)) {
+						parameterValue = line.substring(
+							equalPos + 3, line.length() - 1);
+					}
+					else {
+						parameterValue = line.substring(equalPos + 3);
+					}
+
+					parametersMap.put(parameterName, parameterValue);
+				}
+			}
+
+			StringBundler sb = new StringBundler();
+
+			for (Map.Entry<String, String> entry : parametersMap.entrySet()) {
+				sb.append(indent);
+				sb.append(CharPool.TAB);
+				sb.append(entry.getKey());
+				sb.append(" = ");
+				sb.append(entry.getValue());
+				sb.append(CharPool.COMMA);
+				sb.append(CharPool.NEW_LINE);
+			}
+
+			if (!parametersMap.isEmpty()) {
+				sb.setIndex(sb.index() - 2);
+				sb.append(CharPool.NEW_LINE);
+			}
+
+			content = StringUtil.replaceFirst(
+				content, matcher.group(1), sb.toString(), matcher.start());
+		}
+
+		return content;
+	}
+
+	private static final Pattern _variableNamesPattern = Pattern.compile(
+		"[ \t]*\\w+\\.\\w+\\(\n((([ \t]*\\w+ = .*\n)){2,})[ \t]*\\);\n");
 
 }

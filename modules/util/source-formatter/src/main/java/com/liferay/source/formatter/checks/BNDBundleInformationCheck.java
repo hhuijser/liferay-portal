2f72bc379a46d556a5f6190352c1258c22c213a6
Alan Huang <alan.huang@liferay.com>
2019-01-18 14:21:17 +0800

    LPS-84119 Add BundleActivator check

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDBundleInformationCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/BNDBundleInformationCheck.java
@@ -15,6 +15,7 @@
 package com.liferay.source.formatter.checks;
 
 import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.source.formatter.checks.util.BNDSourceUtil;
 
@@ -36,6 +37,8 @@ public class BNDBundleInformationCheck extends BaseFileCheck {
 			!absolutePath.contains("/testIntegration/") &&
 			!absolutePath.contains("/third-party/")) {
 
+			_checkBundleActivator(fileName, content);
+
 			_checkBundleName(fileName, absolutePath, content);
 
 			String bundleVersion = BNDSourceUtil.getDefinitionValue(
@@ -49,6 +52,49 @@ public class BNDBundleInformationCheck extends BaseFileCheck {
 		}
 
 		return content;
+	}
+
+	private void _checkBundleActivator(String fileName, String content) {
+		String bundleActivator = BNDSourceUtil.getDefinitionValue(
+			content, "Bundle-Activator");
+
+		if (bundleActivator == null) {
+			return;
+		}
+
+		if (!bundleActivator.endsWith("BundleActivator")) {
+			addMessage(
+				fileName,
+				"Incorrect Bundle-Activator, it should end with " +
+					"'BundleActivator'");
+
+			return;
+		}
+
+		int startPos = bundleActivator.lastIndexOf(StringPool.PERIOD);
+		int endPos = bundleActivator.lastIndexOf("BundleActivator");
+
+		String strippedBundleActivator = bundleActivator.substring(
+			startPos + 1, endPos);
+
+		String bundleSymbolicName = BNDSourceUtil.getDefinitionValue(
+			content, "Bundle-SymbolicName");
+
+		if ((strippedBundleActivator != null) && (bundleSymbolicName != null)) {
+			String strippedBundleSymbolicName = StringUtil.replace(
+				bundleSymbolicName, CharPool.PERIOD, StringPool.BLANK);
+
+			if (StringUtil.endsWith(
+					strippedBundleSymbolicName, strippedBundleActivator)) {
+
+				return;
+			}
+		}
+
+		addMessage(
+			fileName,
+			"Incorrect Bundle-Activator, it should match " +
+				"'Bundle-SymbolicName'");
 	}
 
 	private void _checkBundleName(

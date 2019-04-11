f2ee39594c9fd4bd772731b362b8cf9404fcd4af
Hugo Huijser <hugo.huijser@liferay.com>
2019-04-11 13:51:15 +0800

    LPS-12345 SF

--- modules/apps/document-library/document-library-repository-cmis-impl/src/main/java/com/liferay/document/library/repository/cmis/internal/CMISRepository.java
+++ modules/apps/document-library/document-library-repository-cmis-impl/src/main/java/com/liferay/document/library/repository/cmis/internal/CMISRepository.java
@@ -2180,7 +2180,7 @@ public class CMISRepository extends BaseCmisRepository {
 	protected void processException(Exception e) throws PortalException {
 		String message = e.getMessage();
 
-		if ((e instanceof CmisRuntimeException &&
+		if (((e instanceof CmisRuntimeException) &&
 			 message.contains("authorized")) ||
 			(e instanceof CmisPermissionDeniedException)) {
 
@@ -2200,10 +2200,10 @@ public class CMISRepository extends BaseCmisRepository {
 		List<E> list, int start, int end, OrderByComparator<E> obc) {
 
 		if ((obc != null) &&
-			((obc instanceof RepositoryModelCreateDateComparator) ||
-			 (obc instanceof RepositoryModelModifiedDateComparator) ||
-			 (obc instanceof RepositoryModelSizeComparator) ||
-			 (obc instanceof RepositoryModelTitleComparator))) {
+			(obc instanceof RepositoryModelCreateDateComparator ||
+			 obc instanceof RepositoryModelModifiedDateComparator ||
+			 obc instanceof RepositoryModelSizeComparator ||
+			 obc instanceof RepositoryModelTitleComparator)) {
 
 			list = ListUtil.sort(list, obc);
 		}

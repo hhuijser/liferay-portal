8ccc2ab00d4eb668552a6239c6d50c98657f1e1f
Hugo Huijser <hugo.huijser@liferay.com>
2019-05-06 15:34:22 +0800

    LPS-1 Use ListUtil.toList

--- modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/test/util/CalendarStagingTestUtil.java
+++ modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/test/util/CalendarStagingTestUtil.java
@@ -32,9 +32,8 @@ import com.liferay.portal.kernel.service.PortletPreferencesLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ListUtil;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
@@ -99,16 +98,14 @@ public class CalendarStagingTestUtil {
 
 		Group stagingGroup = liveGroup.getStagingGroup();
 
-		List<String> portletIds = new ArrayList<>();
-
-		portletIds.add(CalendarPortletKeys.CALENDAR);
-
 		Map<String, String[]> parameters =
 			ExportImportConfigurationParameterMapFactoryUtil.buildParameterMap(
 				PortletDataHandlerKeys.DATA_STRATEGY_MIRROR_OVERWRITE, true,
 				false, true, false, false, true, true, true, true, false, null,
-				true, false, enableCalendarStaging ? portletIds : null, false,
-				null, ExportImportDateUtil.RANGE_ALL, true, true,
+				true, false,
+				enableCalendarStaging ?
+				ListUtil.toList(CalendarPortletKeys.CALENDAR) : null,
+				false, null, ExportImportDateUtil.RANGE_ALL, true, true,
 				UserIdStrategy.CURRENT_USER_ID);
 
 		StagingUtil.publishLayouts(

14dfd372ad109d7c54240618ff16b221d5c0da9a
Hugo Huijser <hugo.huijser@liferay.com>
2019-05-16 11:03:10 +0800

    LPS-95567 Use 'com.liferay.petra.string.StringBundler' instead of 'com.liferay.portal.kernel.util.StringBundler'

--- modules/util/javadoc-formatter/src/main/java/com/liferay/javadoc/formatter/JavadocFormatter.java
+++ modules/util/javadoc-formatter/src/main/java/com/liferay/javadoc/formatter/JavadocFormatter.java
@@ -16,12 +16,12 @@ package com.liferay.javadoc.formatter;
 
 import com.liferay.javadoc.formatter.util.JavadocFormatterUtil;
 import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.petra.xml.Dom4jUtil;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Tuple;
 import com.liferay.portal.kernel.util.Validator;

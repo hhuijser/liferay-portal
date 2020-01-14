hhuijser/liferay-portal

commit 84d97070edaf307d250a4d900a1973bbf260cf99

LPS-105380 Do not use initials for *Exception parameters

modules/aspectj/aspectj-oracle-driver-socket-timeout/src/main/java/com/liferay/aspectj/oracle/driver/socket/timeout/PacketAspect.java
@@ -33,7 +33,7 @@ public class PacketAspect {
 				"args(interruptedIOException) && this(packet)"
 	)
 	public void addSuppressedInterruptedIOException(
-			Object packet, Exception interruptedIOException)
+			Object packet, Exception exception)
 		throws Exception {
 
 		Class<?> clazz = packet.getClass();
@@ -47,7 +47,7 @@ public class PacketAspect {
 
 		Exception exception = constructor.newInstance(504);
 
-		exception.addSuppressed(interruptedIOException);
+		exception.addSuppressed(exception);
 
 		throw exception;
 	}

commit 7a06507a0221713e704e5e0120d1102c0fdd8e61

LPS-105380 Fix compile (caused by 84d97070edaf307d250a4d900a1973bbf260cf99)

modules/aspectj/aspectj-oracle-driver-socket-timeout/src/main/java/com/liferay/aspectj/oracle/driver/socket/timeout/PacketAspect.java
@@ -33,7 +33,7 @@ public class PacketAspect {
 				"args(interruptedIOException) && this(packet)"
 	)
 	public void addSuppressedInterruptedIOException(
-			Object packet, Exception exception)
+			Object packet, Exception exception1)
 		throws Exception {
 
 		Class<?> clazz = packet.getClass();
@@ -45,11 +45,11 @@ public class PacketAspect {
 		Constructor<? extends Exception> constructor =
 			(Constructor<? extends Exception>)clazz.getConstructor(int.class);
 
-		Exception exception = constructor.newInstance(504);
+		Exception exception2 = constructor.newInstance(504);
 
-		exception.addSuppressed(exception);
+		exception2.addSuppressed(exception1);
 
-		throw exception;
+		throw exception2;
 	}
 
 }
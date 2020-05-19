10453f0bc97a407c9262ed2d33a5d21573a3c362
Hugo Huijser <hugo.huijser@liferay.com>
2020-05-19 15:02:24 +0800

    LPS-105380 Parameterize generic type for Indexer

--- modules/apps/configuration-admin/configuration-admin-test/src/testIntegration/java/com/liferay/configuration/admin/internal/search/test/ConfigurationModelIndexerTest.java
+++ modules/apps/configuration-admin/configuration-admin-test/src/testIntegration/java/com/liferay/configuration/admin/internal/search/test/ConfigurationModelIndexerTest.java
@@ -214,7 +214,7 @@ public class ConfigurationModelIndexerTest {
 	private final List<Document> _documents = new ArrayList<>();
 
 	@Inject(filter = "component.name=*.ConfigurationModelIndexer")
-	private Indexer _indexer;
+	private Indexer<?> _indexer;
 
 	@Inject
 	private IndexWriterHelper _indexWriterHelper;

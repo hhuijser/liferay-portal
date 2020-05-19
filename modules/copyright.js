e43f0cc8efabf4c6b13d54d15bffe9bd2d9d2020
Hugo Huijser <hugo.huijser@liferay.com>
2020-05-19 10:27:08 +0800

    FOSS-74 Use (c) instead of copyright character

--- modules/copyright.js
+++ modules/copyright.js
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: © <%= YEAR %> Liferay, Inc. <https://liferay.com>
+ * SPDX-FileCopyrightText: (c) <%= YEAR %> Liferay, Inc. <https://liferay.com>
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/CopyrightCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checks/CopyrightCheck.java
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-FileCopyrightText: (c) 2020 Liferay, Inc. <https://liferay.com>
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 

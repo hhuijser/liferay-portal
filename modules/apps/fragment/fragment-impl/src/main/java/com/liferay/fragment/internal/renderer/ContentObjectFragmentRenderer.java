5ef80263fcb04ba8dbadd345dbb3d6ef6c9eeed0
Hugo Huijser <hugo.huijser@liferay.com>
2020-05-19 13:22:21 +0800

    LPS-105380 Parameterize generic type for InfoDisplayContributor and InfoDisplayObjectProvider

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/ContentObjectFragmentRenderer.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/ContentObjectFragmentRenderer.java
@@ -157,11 +157,11 @@ public class ContentObjectFragmentRenderer implements FragmentRenderer {
 		String className, long classPK,
 		Optional<Object> displayObjectOptional) {
 
-		InfoDisplayContributor infoDisplayContributor =
+		InfoDisplayContributor<?> infoDisplayContributor =
 			_infoDisplayContributorTracker.getInfoDisplayContributor(className);
 
 		try {
-			InfoDisplayObjectProvider infoDisplayObjectProvider =
+			InfoDisplayObjectProvider<?> infoDisplayObjectProvider =
 				infoDisplayContributor.getInfoDisplayObjectProvider(classPK);
 
 			if (infoDisplayObjectProvider == null) {

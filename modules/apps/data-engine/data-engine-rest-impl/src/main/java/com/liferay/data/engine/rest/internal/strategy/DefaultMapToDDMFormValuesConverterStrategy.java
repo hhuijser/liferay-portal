6358b9f93f7887f4e7a8f7918833007e86e17442
Hugo Huijser <hugo.huijser@liferay.com>
2020-08-31 12:15:06 +0800

    LPS-114165 Add missing @Override

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/strategy/DefaultMapToDDMFormValuesConverterStrategy.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/strategy/DefaultMapToDDMFormValuesConverterStrategy.java
@@ -40,6 +40,7 @@ public class DefaultMapToDDMFormValuesConverterStrategy
 		return _defaultMapToDDMFormValuesConverterStrategy;
 	}
 
+	@Override
 	public void setDDMFormFieldValues(
 		Map<String, Object> dataRecordValues, DDMForm ddmForm,
 		DDMFormValues ddmFormValues, Locale locale) {

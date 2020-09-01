98e186e73d8c42732d9a748ec8c0867e495bf0c7
Hugo Huijser <hugo.huijser@liferay.com>
2020-09-02 07:25:09 +0800

    LPS-1 Only run JavaSourceProcessor

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/SourceFormatter.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/SourceFormatter.java
@@ -314,6 +314,7 @@ public class SourceFormatter {
 
 		_progressStatusThread.start();
 
+		/*
 		_sourceProcessors.add(new BNDRunSourceProcessor());
 		_sourceProcessors.add(new BNDSourceProcessor());
 		_sourceProcessors.add(new CodeownersSourceProcessor());
@@ -342,6 +343,9 @@ public class SourceFormatter {
 		_sourceProcessors.add(new TSSourceProcessor());
 		_sourceProcessors.add(new XMLSourceProcessor());
 		_sourceProcessors.add(new YMLSourceProcessor());
+		*/
+
+		_sourceProcessors.add(new JavaSourceProcessor());
 
 		ExecutorService executorService = Executors.newFixedThreadPool(
 			_sourceProcessors.size());

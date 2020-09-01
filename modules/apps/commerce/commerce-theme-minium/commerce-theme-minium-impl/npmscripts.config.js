a709a565eab8736b6281371d507cb4f54c346bb8
Hugo Huijser <hugo.huijser@liferay.com>
2020-09-01 17:01:47 +0800

    LPS-7

--- modules/apps/commerce/commerce-theme-minium/commerce-theme-minium-impl/npmscripts.config.js
+++
@@ -1,8 +0,0 @@
-const preset = require('liferay-npm-scripts/src/presets/standard');
-
-module.exports = {
-	build: {
-		dependencies: [...preset.build.dependencies, 'commerce-frontend-taglib']
-	},
-	preset: 'liferay-npm-scripts/src/presets/standard'
-};

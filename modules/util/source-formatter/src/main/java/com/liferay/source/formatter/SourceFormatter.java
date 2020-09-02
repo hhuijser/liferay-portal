4e1d6163ada785bdf34afaf2d4747f018e392b14
Hugo Huijser <hugo.huijser@liferay.com>
2020-09-02 08:44:42 +0800

    LPS-1 Add other processors

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/SourceFormatter.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/SourceFormatter.java
@@ -347,6 +347,11 @@ public class SourceFormatter {
 		_sourceProcessors.add(new YMLSourceProcessor());
 		*/
 
+		_sourceProcessors.add(new BNDRunSourceProcessor());
+		_sourceProcessors.add(new BNDSourceProcessor());
+		_sourceProcessors.add(new CodeownersSourceProcessor());
+		_sourceProcessors.add(new ConfigSourceProcessor());
+		_sourceProcessors.add(new CQLSourceProcessor());
 		_sourceProcessors.add(new JavaSourceProcessor());
 
 		ExecutorService executorService = Executors.newFixedThreadPool(

249396f2cf5457544613bf98d7f9d4b7df25dfae
Hugo Huijser <hugo.huijser@liferay.com>
2020-09-17 15:43:26 +0800

    LPS-120746 Generated

--- portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetVocabularyPersistenceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetVocabularyPersistenceImpl.java
@@ -6420,7 +6420,7 @@ public class AssetVocabularyPersistenceImpl
 			return map;
 		}
 
-		StringBundler sb = new StringBundler(primaryKeys.size() * 2 + 1);
+		StringBundler sb = new StringBundler((primaryKeys.size() * 2) + 1);
 
 		sb.append(getSelectSQL());
 		sb.append(" WHERE ");

c32012e1f0a1061b58ec608467749195ba020308
Hugo Huijser <hugo.huijser@liferay.com>
2020-09-28 10:26:16 +0800

    LPS-120746 Generated

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPTaxCategoryModelImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPTaxCategoryModelImpl.java
@@ -1002,7 +1002,7 @@ public class CPTaxCategoryModelImpl
 			getAttributeGetterFunctions();
 
 		StringBundler sb = new StringBundler(
-			4 * attributeGetterFunctions.size() + 2);
+			(4 * attributeGetterFunctions.size()) + 2);
 
 		sb.append("{");
 
@@ -1034,7 +1034,7 @@ public class CPTaxCategoryModelImpl
 			getAttributeGetterFunctions();
 
 		StringBundler sb = new StringBundler(
-			5 * attributeGetterFunctions.size() + 4);
+			(5 * attributeGetterFunctions.size()) + 4);
 
 		sb.append("<model><model-name>");
 		sb.append(getModelClassName());

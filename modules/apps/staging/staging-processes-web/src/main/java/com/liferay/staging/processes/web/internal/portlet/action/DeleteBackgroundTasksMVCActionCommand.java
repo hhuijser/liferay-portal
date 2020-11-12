9c624a734e697bebbb364a4a1c37da8b6ce422d3
Hugo Huijser <hugo.huijser@liferay.com>
2020-11-12 17:22:58 +0800

    LPS-115709 Rename classes

--- modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/internal/portlet/action/DeleteBackgroundTasksMVCActionCommand.java
+++ modules/apps/staging/staging-processes-web/src/main/java/com/liferay/staging/processes/web/internal/portlet/action/DeleteBackgroundTasksMVCActionCommand.java
@@ -42,8 +42,7 @@ import org.osgi.service.component.annotations.Reference;
 	},
 	service = MVCActionCommand.class
 )
-public class DeleteLayoutPublishBackgroundTaskMVCActionCommand
-	extends BaseMVCActionCommand {
+public class DeleteBackgroundTasksMVCActionCommand extends BaseMVCActionCommand {
 
 	protected void deleteBackgroundTask(ActionRequest actionRequest)
 		throws PortalException {

ff7c53c156a06701781cdf06ecaa5b03ccad04ed
Hugo Huijser <hugo.huijser@liferay.com>
2021-01-04 11:02:38 +0100

    LPS-125095 Partial revert of commit 282ab2cd3abade826e160ffc80f335a7f8a27b62

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/servlet/taglib/ui/SelectionStyleFormNavigatorEntry.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/servlet/taglib/ui/SelectionStyleFormNavigatorEntry.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.asset.publisher.web.internal.frontend.taglib.form.navigator;
+package com.liferay.asset.publisher.web.internal.servlet.taglib.ui;
 
 import com.liferay.asset.publisher.constants.AssetPublisherConstants;
 import com.liferay.asset.publisher.web.internal.util.AssetPublisherCustomizer;

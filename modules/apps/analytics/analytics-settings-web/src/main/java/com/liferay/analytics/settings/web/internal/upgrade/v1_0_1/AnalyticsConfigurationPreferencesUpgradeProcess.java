754711352f9686de6352e4a62ee502066c11db7e
Hugo Huijser <hugo.huijser@liferay.com>
2021-03-10 10:35:55 -0700

    LPS-105380 Rename classes: Upgrade* -> *UpgradeProcess

--- modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/upgrade/v1_0_1/AnalyticsConfigurationPreferencesUpgradeProcess.java
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/upgrade/v1_0_1/AnalyticsConfigurationPreferencesUpgradeProcess.java
@@ -32,9 +32,9 @@ import org.osgi.service.cm.ConfigurationAdmin;
 /**
  * @author Shinn Lok
  */
-public class UpgradeAnalyticsConfigurationPreferences extends UpgradeProcess {
+public class AnalyticsConfigurationPreferencesUpgradeProcess extends UpgradeProcess {
 
-	public UpgradeAnalyticsConfigurationPreferences(
+	public AnalyticsConfigurationPreferencesUpgradeProcess(
 		CompanyLocalService companyLocalService,
 		ConfigurationAdmin configurationAdmin) {
 

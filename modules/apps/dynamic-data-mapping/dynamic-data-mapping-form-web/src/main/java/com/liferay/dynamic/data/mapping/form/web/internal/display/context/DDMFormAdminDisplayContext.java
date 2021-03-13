1869b1ed2fef5c2419c09abf39bce3a77e151526
Hugo Huijser <hugo.huijser@liferay.com>
2021-03-13 13:03:01 -0700

    LPS-119446 Use if-statement

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormAdminDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormAdminDisplayContext.java
@@ -1062,7 +1062,11 @@ public class DDMFormAdminDisplayContext {
 				String orderByType = ParamUtil.getString(
 					renderRequest, "orderByType");
 
-				return orderByType.equals("asc") ? "desc" : "asc";
+				if (orderByType.equals("asc")) {
+					return "desc";
+				}
+
+				return "asc";
 			}
 		).buildString();
 	}

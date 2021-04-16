eb35ae33cab7356eb3d31e3a9b717567b4189835
Hugo Huijser <hugo.huijser@liferay.com>
2021-04-16 15:28:36 -0600

    LPS-119446 Improve logic in BaseBuilderCheck

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/BaseBuilderCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/checks/BaseBuilderCheck.java
@@ -296,7 +296,9 @@ public abstract class BaseBuilderCheck extends BaseChainedMethodCheck {
 		while (true) {
 			nextSiblingDetailAST = nextSiblingDetailAST.getNextSibling();
 
-			if (nextSiblingDetailAST == null) {
+			if ((nextSiblingDetailAST == null) ||
+				hasPrecedingPlaceholder(nextSiblingDetailAST)) {
+
 				return;
 			}
 
@@ -323,8 +325,9 @@ public abstract class BaseBuilderCheck extends BaseChainedMethodCheck {
 						return;
 					}
 
-					if (!allowNullValues() &&
-						_isNullValueExpression(childDetailAST)) {
+					if ((!allowNullValues() &&
+						 _isNullValueExpression(childDetailAST)) ||
+						containsVariableName(childDetailAST, variableName)) {
 
 						return;
 					}

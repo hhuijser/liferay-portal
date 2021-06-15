454f4c5f0bf1440ff0850aec3b457b980a24a657
Hugo Huijser <hugo.huijser@liferay.com>
2021-06-15 13:38:48 -0600

    LPS-132180 No need to cast to String when calling setRedirect

--- modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/display/context/AssetCategoriesDisplayContext.java
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/display/context/AssetCategoriesDisplayContext.java
@@ -898,7 +898,7 @@ public class AssetCategoriesDisplayContext {
 		).setMVCPath(
 			"/view.jsp"
 		).setRedirect(
-			currentURL.toString()
+			currentURL
 		).setNavigation(
 			getNavigation()
 		).build();

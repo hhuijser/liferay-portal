6c764ef8cc4b209bd54f357c962d80a1d178627f
Hugo Huijser <hugo.huijser@liferay.com>
2021-07-07 19:40:15 -0700

    LPS-105380 Avoid variable initiation in the constructor if we can do it at the global variable declaration level

--- modules/etl/talend/talend-runtime/src/main/java/com/liferay/talend/runtime/writer/LiferayWriter.java
+++ modules/etl/talend/talend-runtime/src/main/java/com/liferay/talend/runtime/writer/LiferayWriter.java
@@ -59,8 +59,6 @@ public class LiferayWriter
 		_finalEndpointUrl = _getFinalEndpointUrl(_liferayOutputProperties);
 
 		_liferaySink = _liferayWriteOperation.getSink();
-		_result = new Result();
-		_successWrites = new ArrayList<>();
 
 		_indexedRecordJsonObjectConverter =
 			new IndexedRecordJsonObjectConverter(
@@ -258,7 +256,7 @@ public class LiferayWriter
 	private final LiferayOutputProperties _liferayOutputProperties;
 	private final LiferaySink _liferaySink;
 	private final LiferayWriteOperation _liferayWriteOperation;
-	private final Result _result;
-	private final List<IndexedRecord> _successWrites;
+	private final Result _result = new Result();
+	private final List<IndexedRecord> _successWrites = new ArrayList<>();
 
 }

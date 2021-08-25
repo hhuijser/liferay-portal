b2b77eb487d822d3c43bc765f7a58cbc611afc50
Hugo Huijser <hugo.huijser@liferay.com>
2021-08-25 14:34:04 -0600

    LPS-105380 Update usage

--- modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/portlet/action/AutocompleteUserMVCResourceCommand.java
+++ modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/portlet/action/AutocompleteUserMVCResourceCommand.java
@@ -116,7 +116,7 @@ public class AutocompleteUserMVCResourceCommand extends BaseMVCResourceCommand {
 			return Collections.emptyList();
 		}
 
-		return _userLocalService.searchSocial(
+		return _userLocalService.searchBySocial(
 			themeDisplay.getCompanyId(), user.getGroupIds(), query, 0, 20,
 			new UserScreenNameComparator());
 	}

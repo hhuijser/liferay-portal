543002529fe3dbcf9652b07b6e3b59f9930e453c
Hugo Huijser <hugo.huijser@liferay.com>
2021-12-27 11:49:29 -0700

    LPS-105380 Rename methods

--- modules/apps/archived/sync-web/src/main/java/com/liferay/sync/web/internal/portlet/SyncAdminPortlet.java
+++ modules/apps/archived/sync-web/src/main/java/com/liferay/sync/web/internal/portlet/SyncAdminPortlet.java
@@ -75,7 +75,7 @@ public class SyncAdminPortlet extends BaseSyncPortlet {
 		throws IOException, PortletException {
 
 		try {
-			_doUpdatePreferences(actionRequest, actionResponse);
+			_updatePreferences(actionRequest, actionResponse);
 		}
 		catch (Exception exception) {
 			throw new PortletException(exception);
@@ -131,7 +131,7 @@ public class SyncAdminPortlet extends BaseSyncPortlet {
 		_syncOAuthHelperUtil = syncOAuthHelperUtil;
 	}
 
-	private void _doUpdatePreferences(
+	private void _updatePreferences(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
 
